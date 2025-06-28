321310acbb3b151bbba81910659896a31d3a9a2f
Toni Pham <139079071+tonidevvn@users.noreply.github.com>
2025-06-28 01:01:24 -0400

    daily checkin

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Welcome! This README tracks my progress and solutions for LeetCode problems.
 
 ## 📈 Progress
 
-- 😎 Problems Solved: 99
-- ❤️‍🔥 Easy 65/883, 🤔 Med 31/1866, 😑 Hard 3/845
+- 😎 Problems Solved: 104
+- ❤️‍🔥 Easy 68/883, 🤔 Med 33/1866, 😑 Hard 3/845
 - ⌛️ Last Updated: Jun 25, 2025
 
 ## 🚀 How to Use
---
+++ database_problems/177_Nth_Highest_Salary/test.sql
@@ -0,0 +1,64 @@
+/* 
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+*/
+-- Final SQL command --
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N = N - 1;
+  RETURN (
+        select distinct e1.salary from employee e1
+        order by salary desc
+        limit 1 OFFSET N
+  );
+END
